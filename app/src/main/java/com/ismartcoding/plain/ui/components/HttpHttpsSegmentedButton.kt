28d0baee0112443b469ee8d0f663b560dbdfad06
iSmartCoding <ismartcoding@gmail.com>
2026-04-21 22:43:48 +0800

    Enhance code

--- app/src/main/java/com/ismartcoding/plain/ui/components/HttpHttpsSegmentedButton.kt
+++ app/src/main/java/com/ismartcoding/plain/ui/components/HttpHttpsSegmentedButton.kt
@@ -1,11 +1,8 @@
 package com.ismartcoding.plain.ui.components
 
-import android.content.Context
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -16,7 +13,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
-import com.ismartcoding.plain.ui.models.MainViewModel
 import com.ismartcoding.plain.ui.theme.cardBackgroundNormal
 
 @Composable
